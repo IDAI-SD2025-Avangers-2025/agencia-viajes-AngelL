50dd15b5ece8e8d5fcb2abb60e1123302ec579ad
AngelL218 <al45850@un.edu.mx>
2025-11-06 16:35:01 -0600

    Funciones agregadas, excepto dias

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ function calcular(){
     var hospedaje = formulario.hospedaje.value;
     var adultos = formulario.adultos.value
     var menores = formulario.menores.value
-    var diasIndividual = formulario.dias-individual.value
+
+    var diasdiv = document.getElementById("diasdiv").value;
 
     //alert("si funciono")
 
     var total = 0
 
     if(vuelo == "sin-avion") {
-    total = 999;}
+    total = total + 999;}
 
     else if(vuelo == "ida") {
-    total = 2499}
+    total += 2499; }
 
     else if(vuelo == "ida-vuelta") {
-    total = 5999}
+    total += 5999; }
 
     else{
     alert("Por favor selecciona un tipo de vuelo");
@@ -27,18 +28,47 @@ function calcular(){
 
 
     if(hospedaje == "sin-hotel") {
-    total = 499;}
+    total += 499;}
 
     else if(hospedaje == "sencilla") {
-    total = 799}
+    total += 799;}
 
     else if(hospedaje == "doble") {
-    total = 999}
+    total += 999;}
 
     else{
     alert("Por favor selecciona un tipo de hospedaje");
     return; }
 
+
+        if(adultos == "1") {
+    total = total + 865;     }  
+
+        if(adultos == "2") {
+    total = total + 865 * 2;     }      
+
+        if(adultos == "3") {
+    total = total + 865 * 3;     }   
+    
+        if(adultos == "4") {
+    total = total + 865 * 4;     }   
+
+        if(adultos == "5") {
+    total = total + 865 * 5;     }    
+    
+    
+
+        if(menores == "1") {
+    total = total + 437;     }   
+
+        if(menores == "2") {
+    total = total + 437 * 2;     }  
+    
+        if(menores == "3") {
+    total = total + 437 * 3;     }       
+
+        if(menores == "4") {
+    total = total + 437 * 4;     }   
 
     
     formulario.total.value = total;    
